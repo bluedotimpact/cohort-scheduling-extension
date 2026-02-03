c6124a64be6b3e09c5138c422f819f33eaa6495b
Josh Stein <josh.e.stein@gmail.com>
2026-02-03 14:33:04 +0200

    Consistent non-overlapping for dates (left-closed intervals)

--- lib/util.ts
+++ lib/util.ts
@@ -10,7 +10,7 @@ export function newUID() {
 
 /** Check if two date ranges overlap */
 export function dateRangesOverlap(start1: Date, end1: Date, start2: Date, end2: Date): boolean {
-  return start1 <= end2 && start2 <= end1;
+  return start1 < end2 && start2 < end1;
 }
 
 // TODO: move to `weekly-availabilities`
